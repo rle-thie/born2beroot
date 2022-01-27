5f0e2bfdc805180035a97ca71e7338e7cdc317d6
JL Robin <58396524+rle-thie@users.noreply.github.com>
2022-01-27 11:55:15 +0100

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,25 +1,29 @@
-#!/bin/bash
-cpup=`lscpu | grep "CPU(s)" | awk 'NR==1{print $2}'`
-cpuv=`lscpu | grep 'Thread(s) per core:' | awk 'NR==1{print $4}'`
-mem_usage=`free -m | grep 'Mem' | awk '{print $3}'`'/'`free -m | grep 'Mem' | awk '{print $2}'`'MB ('`free -m | grep 'Mem' | awk '{print $3 * 100 / $2}' | cut -c-4`'%)'
-disk_total=`df -m | sed 1d | awk '{sum+=$2;}END{print sum}' | cut -c-1`
-disk_usage=`df -m | sed 1d | awk '{sum+=$3;}END{print sum}'`
-disk_percent=`df | sed 1d | awk '{sum+=$2; sum2+=$3}END{print sum2 * 100 / sum}' | cut -c-2`
-cpu_load=`mpstat | grep "all" | awk '{print 100-$13}'`
-lvm_use=`lsblk | grep lvm | head -n1 | awk '{if($6 == "lvm") print "yes"; else print "no"}'` 
-tcp_connexion=`netstat -an | grep ESTABLISHED | wc -l`
-ip=`hostname -I` 
-mac=`ip link | grep "link/ether" | awk '{print $2}'`
-sudo_cmd=`grep COMMAND /var/log/sudo/sudo.log | wc -l`
-wall $'#Architecture : '`uname -a` \
-$'\n#CPU physical : '$cpup \
-$'\n#vCPU : '$cpuv \
-$'\n#Memory Usage : '$mem_usage \
-$'\n#Disk Usage : '$disk_usage"/"$disk_total"GB ("$disk_percent"%)" \
-$'\n#CPU load : ' $cpu_load"%"\
-$'\n#Last boot : '`who -b | awk '{print $3,$4}'` \
-$'\n#LVM use : '$lvm_use \
-$'\n#Connexions TCP : '$tcp_connexion "ESTABLISHED" \
-$'\n#User log : '`users | wc -w` \
-$'\n#Network : '"IP" $ip "("$mac")" \
-$'\n#Sudo : ' $sudo_cmd "cmd" \
+CPU_PHYSICAL=`grep -c ^processor /proc/cpuinfo`
+vCPU=`lscpu | grep "Thread(s)" | awk '{print $4}'`
+MEM_PERC=`free -m | grep "Mem" | awk '{print $3/$2*100}'`
+MEM_USE=`free -m | grep "Mem" | awk '{print $3"/"$2}'`
+DISK_USE=`df -m | awk '{sum+=$3}; END {print sum}'`
+DISK_TOTAL=`df -m | awk '{sum+=$2}; END {print sum}'`
+DISK_PERC=`df -m | awk '{sum+=$3}; {sum2+=$2}; END {print sum/sum2*100}'`
+CPU_PERC=`mpstat | grep "all" | awk '{print $4}'`
+LAST_BOOT=`uptime -s`
+WC_LVM=`lsblk | grep lvm | wc -l`
+LVM_USE=`if [ $WC_LVM -eq 0 ]; then echo no; else echo yes; fi`
+TCP_USE=`ss -s | grep TCP: | awk '{print ($4)+0}'`
+USER_LOG=`who | wc -l`
+IPv4=`hostname -I | awk '{print $1}'`
+MAC=`ip a | grep link/ether | awk '{print $2}'`
+SUDO_NB=`cat /var/log/sudo/sudo.log | grep COMMAND | wc -l`
+
+wall "#Architeture : $ARCHI
+#CPU Physical : $CPU_PHYSICAL
+#vCPU : $vCPU
+#Memory Usage : $MEM_USE MB (${MEM_PERC:00:4}%)
+#Disk Usage : $DISK_USE/$DISK_TOTAL MB (${DISK_PERC:0:4}%)
+#CPU Load : $CPU_PERC%
+#Last Boot : $LAST_BOOT
+#LVM Use : $LVM_USE
+#Connexions TCP : $TCP_USE ESTABLISHED
+#User log : $USER_LOG
+#Network : IP $IPv4 ($MAC)
+#Sudo : $SUDO_NB cmd"
